622bfcea830dd388096d29f1ead27bb96f0a8040
Dev <kanuj2151@gmail.com>
2025-05-05 15:56:54 +0530

    logo image updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '/pokeapi.png';
 
 const Header = () => {
   return (
     <header className="bg-teal-600 text-white p-4 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <div className='flex flex-row gap-4'>
-          <img className='w-20 ' src="/pokeapi.png" alt="logo" />
+          <img className='w-20 ' src={logo} alt="logo" />
           <h1 className="text-2xl font-bold">Pokemon</h1>
         </div>
         <p className="text-sm hidden sm:block">Explore and search your favorite Pokémon.</p>
